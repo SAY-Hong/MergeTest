9140d6c1655186d6b74f47a63232a7cde8c69017
SAY-Hong <bobseahee@gmail.com>
2023-12-11 01:09:36 +0900

    Add: View2

---
+++ TestProjects/Data/DataTestView.swift
@@ -0,0 +1,18 @@
+//
+//  DataTestView.swift
+//  TestProjects
+//
+//  Created by 홍세희 on 2023/12/11.
+//
+
+import SwiftUI
+
+struct DataTestView: View {
+    var body: some View {
+        Text("테스트2")
+    }
+}
+
+#Preview {
+    DataTestView()
+}
