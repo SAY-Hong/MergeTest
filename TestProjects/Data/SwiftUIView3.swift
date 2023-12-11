20b939417949a7dcae488a08cb65ec915e2f807c
SAY-Hong <bobseahee@gmail.com>
2023-12-12 01:23:16 +0900

    test

---
+++ TestProjects/Data/SwiftUIView3.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView3.swift
+//  TestProjects
+//
+//  Created by 홍세희 on 2023/12/11.
+//
+
+import SwiftUI
+
+struct SwiftUIView3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView3()
+}
