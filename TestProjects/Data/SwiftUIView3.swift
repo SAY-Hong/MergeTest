0d2c86fdd3cd1a660f91c47a48d7778545fc062e
SAY-Hong <bobseahee@gmail.com>
2024-01-15 10:36:58 +0900

    Edit: 문구 수정 #5

--- TestProjects/Data/SwiftUIView3.swift
+++ TestProjects/Data/SwiftUIView3.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct SwiftUIView3: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Bye world!")
     }
 }
 
